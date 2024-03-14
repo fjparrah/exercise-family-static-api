d563b155300c878399f4e89afdddeb47c6098556
fjparrah <48214359+fjparrah@users.noreply.github.com>
2024-03-14 16:28:02 +0000

    actualizacion

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 jackson_family = Family('Jackson')
 
 @app.route('/members', methods=['GET'])
-def get_all_members():
+def get_members():
     return jsonify(jackson_family.get_all_members()), 200
 
 @app.route('/member/<int:member_id>', methods=['GET'])
@@ -14,25 +14,24 @@ def get_member(member_id):
     if member:
         return jsonify(member), 200
     else:
-        return jsonify({'error': 'Member not found'}), 404
+        return jsonify({"error": "Miembro no encontrado"}), 400
 
 @app.route('/member', methods=['POST'])
 def add_member():
-    try:
-        data = request.get_json()
-        jackson_family.add_member(data)
+    member_data = request.json
+    if 'first_name' in member_data and 'age' in member_data and 'lucky_numbers' in member_data:
+        jackson_family.add_member(member_data)
         return jsonify({}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    else:
+        return jsonify({"error": "Campos requeridos faltantes"}), 400
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
-    try:
-        jackson_family.delete_member(member_id)
-        return jsonify({'done': True}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    if jackson_family.delete_member(member_id):
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"error": "Miembro no encontrado"}), 400
 
-# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     app.run(debug=True)
+
--- src/datastructures.py
+++ src/datastructures.py
@@ -4,52 +4,54 @@ class Family:
 
     def __init__(self, last_name):
         self.last_name = last_name
-        self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            },
-            {
-                "id": self._generate_id(),
-                "first_name": "Jane",
-                "last_name": last_name,
-                "age": 35,
-                "lucky_numbers": [10, 14, 3]
-            },
-            {
-                "id": self._generate_id(),
-                "first_name": "Jimmy",
-                "last_name": last_name,
-                "age": 5,
-                "lucky_numbers": [1]
-            }
-        ]
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        }, {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        }, {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": last_name,
+            "age": 5,
+            "lucky_numbers": [1]
+        }]
 
-    def _generate_id(self):
+    def _generateId(self):
         return random.randint(0, 99999999)
 
     def add_member(self, member):
-        member["id"] = member.get("id", self._generate_id())
-        member["last_name"] = self.last_name
+        if 'id' not in member:
+            member['id'] = self._generateId()
         self._members.append(member)
 
-    def delete_member(self, member_id):
-        self._members = [m for m in self._members if m["id"] != member_id]
+    def delete_member(self, id):
+        for i, member in enumerate(self._members):
+            if member['id'] == id:
+                del self._members[i]
+                return True
+        return False
 
-    def update_member(self, member_id, member):
-        for i, m in enumerate(self._members):
-            if m["id"] == member_id:
+    def update_member(self, id, member):
+        for i, mem in enumerate(self._members):
+            if mem['id'] == id:
                 self._members[i] = member
-                break
+                return True
+        return False
 
-    def get_member(self, member_id):
+    def get_member(self, id):
         for member in self._members:
-            if member["id"] == member_id:
+            if member['id'] == id:
                 return member
         return None
 
     def get_all_members(self):
         return self._members
+
